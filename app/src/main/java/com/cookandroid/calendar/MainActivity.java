2ec3b829e8881d17f508aef0cd5027cf18390057
Cluster-Taek <yst811@naver.com>
2020-05-18 14:36:53 +0900

    Custom CalendarView 예제

---
+++ app/src/main/java/com/cookandroid/calendar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cookandroid.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void setCalendarList() {
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
